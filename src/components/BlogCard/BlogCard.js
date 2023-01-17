3c0f5e0636335d35a9e749cce5454dc60ab8f4ed
Hana Acciaioli <hana.acciaioli@gmail.com>
2023-01-17 14:31:14 -0800

    stretch goal complete: author's name displaying

--- src/components/BlogCard/BlogCard.js
+++ src/components/BlogCard/BlogCard.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import { checkError } from '../../services/client.js';
 import './BlogCard.css';
 
-export default function BlogCard({ title, subtitle, text, image }) {
+export default function BlogCard({ title, subtitle, text, image, authors: { name } }) {
   return (
     <div className="blog-card">
       <div className="details">
         <div className="title">{title}</div>
         <div className="subtitle">{subtitle}</div>
         <div className="text">{text}</div>
+        <duv className="name">{name}</duv>
       </div>
       <div className="image">
         <img src={image} />
--- src/services/getBlogs.js
+++ src/services/getBlogs.js
@@ -1,6 +1,9 @@
 import { checkError, client } from './client';
 
 export async function getBlogs() {
-  const resp = await client.from('blogs').select('*');
+  const resp = await client.from('blogs').select(`
+  *,
+  authors("*")
+`);
   return checkError(resp);
 }
