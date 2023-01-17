16c2b9fbde7c18458b2e22bd242a1d5af6c272aa
Hana Acciaioli <hana.acciaioli@gmail.com>
2023-01-17 12:52:08 -0800

    useEffect in useBlogs hook correctly calls supabase when the component is first mounted & each blog is correctly rendered using the BlogCard component

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import './Main.css';
+import BlogCard from '../BlogCard/BlogCard';
+import { useBlogs } from '../../hooks/useBlogs';
 
 export default function Main() {
-  return <div>Main</div>;
+  const blogs = useBlogs();
+  return (
+    <main>
+      {blogs.map((blog) => (
+        <BlogCard key={blogs.id} {...blog} />
+      ))}
+    </main>
+  );
 }
